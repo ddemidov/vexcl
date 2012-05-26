a5d6d49091ab58ecb1f088e160a330d321dbe50d
Denis Demidov <ddemidov@ksu.ru>
2012-05-26 23:25:07 +0400

    Device info in examples/devlist.cpp

--- examples/devlist.cpp
+++ examples/devlist.cpp
@@ -3,6 +3,16 @@
 using namespace clu;
 
 int main() {
-    std::cout << "OpenCL devices:" << std::endl;
-    auto dev = device_list(Filter::All(), true);
+    std::cout << "OpenCL devices:" << std::endl << std::endl;
+    auto dev = device_list();
+    for (auto d = dev.begin(); d != dev.end(); d++) {
+	std::cout << "  " << d->getInfo<CL_DEVICE_NAME>() << std::endl
+		  << "    CL_DEVICE_OPENCL_C_VERSION    = " << d->getInfo<CL_DEVICE_OPENCL_C_VERSION>() << std::endl
+		  << "    CL_DEVICE_MAX_COMPUTE_UNITS   = " << d->getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << std::endl
+	          << "    CL_DEVICE_HOST_UNIFIED_MEMORY = " << d->getInfo<CL_DEVICE_HOST_UNIFIED_MEMORY>() << std::endl
+		  << "    CL_DEVICE_GLOBAL_MEM_SIZE     = " << d->getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>() << std::endl
+		  << "    CL_DEVICE_LOCAL_MEM_SIZE      = " << d->getInfo<CL_DEVICE_LOCAL_MEM_SIZE>() << std::endl
+		  << "    CL_DEVICE_MAX_CLOCK_FREQUENCY = " << d->getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>() << std::endl
+		  << std::endl;
+    }
 }
