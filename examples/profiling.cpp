ee191c74d14fa2afe6718c9a0ac523d643250985
Denis Demidov <ddemidov@ksu.ru>
2012-05-30 14:41:37 +0400

    SpMV profiling

--- examples/profiling.cpp
+++ examples/profiling.cpp
@@ -112,6 +112,119 @@ void benchmark_reductor(std::vector<cl::CommandQueue> &queue, profiler &prof)
 	      << std::endl << std::endl;
 }
 
+void benchmark_spmv(std::vector<cl::CommandQueue> &queue, profiler &prof) {
+    // Construct matrix for 3D Poisson problem in cubic domain.
+    const uint n = 128;
+    const uint N = n * n * n;
+    const uint M = 1024;
+
+    double time_elapsed;
+
+    const real h   = 1.0 / (n - 1);
+    const real h2i = (n - 1) * (n - 1);
+
+    std::vector<uint> row;
+    std::vector<uint> col;
+    std::vector<real> val;
+    std::vector<real> X(n * n * n, 1e-2);
+    std::vector<real> Y(n * n * n, 0);
+
+    row.reserve(n * n * n + 1);
+    col.reserve(6 * (n - 2) * (n - 2) * (n - 2) + n * n * n);
+    val.reserve(6 * (n - 2) * (n - 2) * (n - 2) + n * n * n);
+
+    row.push_back(0);
+    for(uint k = 0, idx = 0; k < n; k++) {
+	real z = k * h;
+	for(uint j = 0; j < n; j++) {
+	    real y = j * h;
+	    for(uint i = 0; i < n; i++, idx++) {
+		real x = i * h;
+		if (
+			i == 0 || i == (n - 1) ||
+			j == 0 || j == (n - 1) ||
+			k == 0 || k == (n - 1)
+		   )
+		{
+		    col.push_back(idx);
+		    val.push_back(1);
+		    row.push_back(row.back() + 1);
+		} else {
+		    col.push_back(idx - n * n);
+		    val.push_back(-h2i);
+
+		    col.push_back(idx - n);
+		    val.push_back(-h2i);
+
+		    col.push_back(idx - 1);
+		    val.push_back(-h2i);
+
+		    col.push_back(idx);
+		    val.push_back(6 * h2i);
+
+		    col.push_back(idx + 1);
+		    val.push_back(-h2i);
+
+		    col.push_back(idx + n);
+		    val.push_back(-h2i);
+
+		    col.push_back(idx + n * n);
+		    val.push_back(-h2i);
+
+		    row.push_back(row.back() + 7);
+		}
+	    }
+	}
+    }
+
+    uint nnz = row.back();
+
+    // Transfer data to compute devices.
+    vex::SpMat<real>  A(queue, n * n * n, row.data(), col.data(), val.data());
+    vex::vector<real> x(queue, CL_MEM_READ_WRITE, X);
+    vex::vector<real> y(queue, CL_MEM_READ_WRITE, Y);
+
+    // Get timings.
+    y += A * x;
+    y = Const(0);
+
+    prof.tic_cl("OpenCL");
+    for(uint i = 0; i < M; i++)
+	y += A * x;
+    time_elapsed = prof.toc("OpenCL");
+
+    std::cout << "SpMV\n"
+              << "  OpenCL\n    GFLOPS:    "
+	      << 15.0 * N * M / time_elapsed / 1e9
+              << "\n    Bandwidth: "
+	      << M * (7 * N * (2 * sizeof(real) + sizeof(uint)) + 2 * N * sizeof(real)) / time_elapsed / 1e9
+	      << std::endl;
+
+    prof.tic_cpu("C++");
+    for(size_t k = 0; k < M; k++)
+	for(uint i = 0; i < N; i++) {
+	    real s = 0;
+	    for(uint j = row[i]; j < row[i + 1]; j++)
+		s += val[j] * X[col[j]];
+	    Y[i] += s;
+	}
+    time_elapsed = prof.toc("C++");
+
+    std::cout << "  C++\n    GFLOPS:    "
+	      << (2.0 * nnz + N) * M / time_elapsed / 1e9
+              << "\n    Bandwidth: "
+	      << M * (nnz * (2 * sizeof(real) + sizeof(uint)) + 4 * N * sizeof(real)) / time_elapsed / 1e9
+	      << std::endl;
+
+    copy(Y, x);
+
+    y -= x;
+
+    Reductor<real,SUM> sum(queue);
+
+    std::cout << "  res = " << sum(y * y) << std::endl << std::endl;
+}
+
 int main() {
     try {
 	const char *platform = getenv("OCL_PLATFORM");
@@ -137,6 +250,10 @@ int main() {
 	prof.tic_cpu("Reduction");
 	benchmark_reductor(queue, prof);
 	prof.toc("Reduction");
+
+	prof.tic_cpu("SpMV");
+	benchmark_spmv(queue, prof);
+	prof.toc("SpMV");
 
 	std::cout << prof << std::endl;
     } catch (const cl::Error &e) {
