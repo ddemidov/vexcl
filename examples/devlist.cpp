a5afd32968c0517d6ca43093a76bdcdf40b8ce9a
Denis Demidov <ddemidov@ksu.ru>
2012-09-19 14:03:53 +0400

    Removed CL/cl.hpp
    The khronos header does not work with AMDAPP OpenCL SDK. VexCL should work with
    vendor-provided headers without any problems. NVIDIA does not include cl.hpp in
    CUDA Toolkit for Windows, so it has to be downloaded manually.

--- examples/devlist.cpp
+++ examples/devlist.cpp
@@ -8,7 +8,6 @@ int main() {
     for (auto d = dev.begin(); d != dev.end(); d++) {
 	std::cout << "  " << d->getInfo<CL_DEVICE_NAME>() << std::endl
 		  << "    CL_PLATFORM_NAME              = " << cl::Platform(d->getInfo<CL_DEVICE_PLATFORM>()).getInfo<CL_PLATFORM_NAME>() << std::endl
-		  << "    CL_DEVICE_OPENCL_C_VERSION    = " << d->getInfo<CL_DEVICE_OPENCL_C_VERSION>() << std::endl
 		  << "    CL_DEVICE_MAX_COMPUTE_UNITS   = " << d->getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << std::endl
 	          << "    CL_DEVICE_HOST_UNIFIED_MEMORY = " << d->getInfo<CL_DEVICE_HOST_UNIFIED_MEMORY>() << std::endl
 		  << "    CL_DEVICE_GLOBAL_MEM_SIZE     = " << d->getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>() << std::endl
