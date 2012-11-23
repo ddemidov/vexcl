2bf6103a7e09171b224aec56036e43d4dbc48cf2
Denis Demidov <ddemidov@ksu.ru>
2012-11-23 11:21:30 +0400

    Added bicgstab example

--- examples/viennacl/solvers.cpp
+++ examples/viennacl/solvers.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <vector>
+
 #include <vexcl/vexcl.hpp>
 #include <vexcl/external/viennacl.hpp>
+
 #include <viennacl/linalg/cg.hpp>
+#include <viennacl/linalg/bicgstab.hpp>
 
 #include "genproblem.hpp"
 
 typedef double real;
+
+template <class Tag>
+void do_solve(const vex::SpMat<real> &A, vex::vector<real> f, const Tag &tag)
+{
+    vex::vector<real> x = viennacl::linalg::solve(A, f, tag);
+
+    std::cout << "  Iterations: " << tag.iters() << std::endl
+              << "  Error:      " << tag.error() << std::endl;
+
+    // Test for convergence.
+    f -= A * x;
+
+    static vex::Reductor<real, vex::MAX> max(vex::current_context().queue());
+    std::cout << "  max(residual) = " << max(fabs(f)) << std::endl;
+}
+
 int main() {
     try {
         // Prepare problem (1D Poisson equation).
@@ -30,16 +49,12 @@ int main() {
         vex::SpMat <real> A(ctx.queue(), n, n, row.data(), col.data(), val.data());
         vex::vector<real> f(ctx.queue(), rhs);
 
-        // Solve problem with ViennaCL's CG implementation.
-        vex::vector<real> x = viennacl::linalg::solve(
-                A, f, viennacl::linalg::cg_tag(1e-8, n)
-                );
+        // Solve problem with ViennaCL's solvers:
+        std::cout << "CG" << std::endl;
+        do_solve(A, f, viennacl::linalg::cg_tag(1e-8, n));
 
-        // Test for convergence.
-        f -= A * x;
-
-        vex::Reductor<real, vex::MAX> max(ctx.queue());
-        std::cout << "res = " << max(fabs(f)) << std::endl;
+        std::cout << "BiCGStab" << std::endl;
+        do_solve(A, f, viennacl::linalg::bicgstab_tag(1e-8, n));
 
     } catch(const cl::Error &err) {
         std::cerr << "OpenCL Error: " << err << std::endl;
