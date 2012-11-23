9247a63a657ec395eb708881aab9bb088a655c1e
Denis Demidov <ddemidov@ksu.ru>
2012-11-23 10:37:52 +0400

    Moved viennacl example(s) to a subdirectory
    The examples are compiled only if requested

--- examples/viennacl/cg.cpp
+++ examples/viennacl/cg.cpp
@@ -1,49 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
 #include <vexcl/vexcl.hpp>
 #include <vexcl/external/viennacl.hpp>
 #include <viennacl/linalg/cg.hpp>
 
-typedef double real;
+#include "genproblem.hpp"
 
+typedef double real;
 int main() {
     try {
-        size_t n = 1024;
-        real h = 1.0 / (n - 1);
-
         // Prepare problem (1D Poisson equation).
+        size_t n = 1024;
+
         std::vector<size_t> row;
         std::vector<size_t> col;
         std::vector<real>   val;
         std::vector<real>   rhs;
 
-        row.reserve(n + 1);
-        col.reserve(2 + (n - 2) * 3);
-        val.reserve(2 + (n - 2) * 3);
-        rhs.reserve(n);
-
-        row.push_back(0);
-        for(size_t i = 0; i < n; i++) {
-            if (i == 0 || i == n-1) {
-                col.push_back(i);
-                val.push_back(1);
-                rhs.push_back(0);
-                row.push_back(row.back() + 1);
-            } else {
-                col.push_back(i-1);
-                val.push_back(-1/(h*h));
-
-                col.push_back(i);
-                val.push_back(2/(h*h));
-
-                col.push_back(i+1);
-                val.push_back(-1/(h*h));
-
-                rhs.push_back(2);
-                row.push_back(row.back() + 3);
-            }
-        }
+        genproblem(n, row, col, val, rhs);
 
         // Move data to GPUs.
         vex::Context ctx(
