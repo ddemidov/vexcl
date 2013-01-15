55dda10974c29a4d1c37c3960ce5521c6e5eb19b
Denis Demidov <ddemidov@ksu.ru>
2013-01-15 11:38:02 +0400

    License years, file names for doxygen in fft folder

--- vexcl/fft/kernels.hpp
+++ vexcl/fft/kernels.hpp
@@ -4,7 +4,7 @@
 /*
 The MIT License
 
-Copyright (c) 2012 Denis Demidov <ddemidov@ksu.ru>
+Copyright (c) 2012-2013 Denis Demidov <ddemidov@ksu.ru>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@ THE SOFTWARE.
 */
 
 /**
- * \file   kernels.hpp
+ * \file   fft/kernels.hpp
  * \author Pascal Germroth <pascal@ensieve.org>
  * \brief  Kernel generator for FFT.
  */
