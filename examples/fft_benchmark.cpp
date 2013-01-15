4285a4b3716c33b4d5ac848715d7b6e5e57a9f7f
Pascal Germroth <pascal@ensieve.org>
2013-01-15 14:11:42 +0100

    Output performance in benchmark, FFTw run optional

--- examples/fft_benchmark.cpp
+++ examples/fft_benchmark.cpp
@@ -2,28 +2,16 @@
 #include <vexcl/fft.hpp>
 #include <random>
 
-#ifdef USE_OPENMP
-#include <omp.h>
-#endif
-
-#include <fftw3.h>
 using namespace vex;
 
-
-const float range = 1000;
 const size_t runs = 1000;
 
 
-
-
-
 #ifdef USE_CUDA
-
 #include <cufft.h>
 #include <cuda_runtime.h>
 
-// Run complex to complex X[2*N] to Y[2*N].
-void test_cufft(cl_float2 *data, size_t n, size_t m) {
+double test_cufft(cl_float2 *data, size_t n, size_t m) {
     size_t dataSize = sizeof(cufftComplex) * n * m;
     cudaError_t status;
     cufftResult fftStatus;
@@ -47,22 +35,32 @@ void test_cufft(cl_float2 *data, size_t n, size_t m) {
     status = cudaMemcpy(inData, data, dataSize, cudaMemcpyHostToDevice);
     assert(status == cudaSuccess);
 
-    cudaDeviceSynchronize();
     profiler prof({});
     prof.tic_cpu("Run");
     for(size_t i = 0 ; i < runs ; i++)
         cufftExecC2C(plan, inData, outData, CUFFT_FORWARD);
     cudaDeviceSynchronize();
-    std::cout << '\t' << prof.toc("Run") << std::flush;
+    double t = prof.toc("Run");
 
     cufftDestroy(plan);
     cudaFree(inData);
     cudaFree(outData);
+    return t;
+}
+#else
+double test_cuda(cl_float2 *, size_t, size_t) {
+    return -1;
 }
 #endif
 
 
-void test_fftw(cl_float2 *data, size_t n, size_t m) {
+#ifdef USE_FFTW
+#ifdef USE_OPENMP
+#include <omp.h>
+#endif
+#include <fftw3.h>
+
+double test_fftw(cl_float2 *data, size_t n, size_t m) {
     int sz[2] = {(int)n, (int)m};
     fftwf_complex *out = reinterpret_cast<fftwf_complex *>(
         fftwf_malloc(sizeof(fftwf_complex) * n * m));
@@ -74,14 +72,20 @@ void test_fftw(cl_float2 *data, size_t n, size_t m) {
     prof.tic_cpu("Run");
     for(size_t i = 0 ; i < runs ; i++)
         fftwf_execute(p1);
-    std::cout << '\t' << prof.toc("Run") << std::flush;
+    double t = prof.toc("Run");
 
     fftwf_destroy_plan(p1);
     fftwf_free(out);
+    return t;
 }
+#else
+double test_fftw(cl_float2 *, size_t, size_t) {
+    return -1;
+}
+#endif
 
 
-void test(Context &ctx, cl_float2 *data, size_t n, size_t m) {
+double test(Context &ctx, cl_float2 *data, size_t n, size_t m) {
     vector<cl_float2> a(ctx.queue(), n * m, data);
     vector<cl_float2> b(ctx.queue(), n * m);
     std::vector<size_t> sz; sz.push_back(n); if(m > 1) sz.push_back(m);
@@ -94,50 +98,63 @@ void test(Context &ctx, cl_float2 *data, size_t n, size_t m) {
     for(size_t i = 0 ; i < runs ; i++)
         b = fft(a);
     ctx.queue()[0].finish();
-    std::cout << '\t' << prof.toc("Run") << std::flush;
+    return prof.toc("Run");
+}
+
+void info(double time, size_t size, size_t dim) {
+    // FFT is O(n log n)
+    double ops = dim == 1
+        ? size * std::log(size) // O(n log n)
+        : 2.0 * size * size * std::log(size); // O(n log n)[1D fft] * n[rows] * 2[transposed]
+    std::cout << '\t';
+    if(time < 0) std::cout << '-';
+    else std::cout << (ops / time);
+    std::cout << std::flush;
 }
 
 int main() {
-#ifdef USE_OPENMP
+#if defined(USE_OPENMP) && defined(USE_FFTW)
     fftwf_init_threads();
     fftwf_plan_with_nthreads(omp_get_max_threads());
 #endif
     Context ctx(Filter::Count(1), CL_QUEUE_PROFILING_ENABLE);
 
     // random data
-    const size_t k_max = 9, n_max = 1 << k_max;
+    const size_t k_max = 10, n_max = 1 << k_max;
     const size_t max_len = n_max * n_max;
+#ifdef USE_FFTW
     cl_float2 *data = reinterpret_cast<cl_float2 *>(
         fftwf_malloc(sizeof(cl_float2) * max_len));
+#else
+    cl_float2 *data = new cl_float2[max_len];
+#endif
     std::minstd_rand gen;
-    std::uniform_real_distribution<float> dist(-range, range);
+    std::uniform_real_distribution<float> dist(-1000, 1000);
     for(size_t i = 0 ; i < max_len ; i++)
         reinterpret_cast<float*>(data)[i] = dist(gen);
 
+    std::cout << "# prints `n log n / time` for n = 2^k\n";
+
     // 1D
-    std::cout << "#k\tt(fftw1)\tt(clfft1)\tt(cufft1)" << std::endl;
+    std::cout << "#n\tfftw^1\tclfft^1\tcufft^1" << std::endl;
     for(size_t k = 1 ; k < k_max * 2 ; k++) {
         size_t n = 1 << k;
-        std::cout << k;
-        test_fftw(data, n, 1);
-        test(ctx, data, n, 1);
-        #ifdef USE_CUDA
-        test_cufft(data, n, 1);
-        #endif
+        std::cout << n;
+        info(test_fftw(data, n, 1), n, 1);
+        info(test(ctx, data, n, 1), n, 1);
+        info(test_cufft(data, n, 1), n, 1);
         std::cout << std::endl;
     }
     std::cout << "\n\n";
 
     // 2D
-    std::cout << "#k\tt(fftw2)\tt(clfft2)\t(cufft2)" << std::endl;
+    std::cout << "#n\tfftw^2\tclfft^2\tcufft^2" << std::endl;
     for(size_t k = 1 ; k < k_max ; k++) {
         size_t n = 1 << k;
-        std::cout << k;
-        test_fftw(data, n, n);
-        test(ctx, data, n, n);
-        #ifdef USE_CUDA
-        test_cufft(data, n, n);
-        #endif
+        std::cout << n;
+        info(test_fftw(data, n, n), n, 2);
+        info(test(ctx, data, n, n), n, 2);
+        info(test_cufft(data, n, n), n, 2);
         std::cout << std::endl;
     }
 
