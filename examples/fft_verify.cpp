9317620dcadfd953a6c2f0f06ea9903603da2047
Pascal Germroth <pascal@ensieve.org>
2013-02-14 00:13:01 +0100

    Verify float/double CLFFT against double FFTw

--- examples/fft_verify.cpp
+++ examples/fft_verify.cpp
@@ -5,20 +5,17 @@
 
 using namespace vex;
 
-float hsum(cl_float2 a) { return a.s[0] + a.s[1]; }
+typedef double T;
+typedef cl_vector_of<T, 2>::type T2;
 
-
-#include <random>
-
-std::vector<cl_float2> random_vec(size_t n, float range) {
-    std::vector<cl_float2> data(n);
-    std::default_random_engine gen;
-    std::uniform_real_distribution<float> dist(-range, range);
-    for(size_t i = 0 ; i < n ; i++)
-        data[i] = cl_float2{{dist(gen), dist(gen)}};
+std::vector<cl_double2> random_vec(size_t n) {
+    std::vector<cl_double2> data(n);
+    for(size_t i = 0 ; i < n ; i++) {
+        data[i].s[0] = 1.0 * rand() / RAND_MAX - 0.5;
+        data[i].s[1] = 1.0 * rand() / RAND_MAX - 0.5;
+    }
     return data;
 }
-
 
 
 bool test(Context &ctx, std::vector<size_t> ns) {
@@ -34,49 +31,55 @@ bool test(Context &ctx, std::vector<size_t> ns) {
     size_t n = 1;
     for(size_t i = 0 ; i < ns.size() ; i++) n *= ns[i];
 
-    const float range = 1000;
-
     // random data.
-    std::vector<cl_float2> input_h = random_vec(n, range);
+    std::vector<cl_double2> input_h = random_vec(n);
 
     // reference.
-    std::vector<cl_float2> ref_h(n);
+    std::vector<cl_double2> ref_h(n);
     std::vector<int> ns_(ns.begin(), ns.end());
-    fftwf_plan p1 = fftwf_plan_dft(ns_.size(), ns_.data(),
-        reinterpret_cast<fftwf_complex *>(&input_h[0]),
-        reinterpret_cast<fftwf_complex *>(&ref_h[0]),
+    fftw_plan p1 = fftw_plan_dft(ns_.size(), ns_.data(),
+        reinterpret_cast<fftw_complex *>(&input_h[0]),
+        reinterpret_cast<fftw_complex *>(&ref_h[0]),
         FFTW_FORWARD, FFTW_ESTIMATE);
-    fftwf_execute(p1);
-    fftwf_destroy_plan(p1);
+    fftw_execute(p1);
+    fftw_destroy_plan(p1);
+
+    // convert to clFFT precision
+    std::vector<T2> input_h_t(n), ref_h_t(n);
+    for(size_t i = 0; i != n ; i++) {
+        input_h_t[i] = cl_convert<T2>(input_h[i]);
+        ref_h_t[i] = cl_convert<T2>(ref_h[i]);
+    }
 
     // test
-    vector<cl_float2> input(ctx.queue(), input_h);
-    vector<cl_float2> output(ctx.queue(), n);
-    vector<cl_float2> ref(ctx.queue(), ref_h);
-    vector<cl_float2> back(ctx.queue(), n);
+    vector<T2> input(ctx.queue(), input_h_t);
+    vector<T2> output(ctx.queue(), n);
+    vector<T2> ref(ctx.queue(), ref_h_t);
+    vector<T2> back(ctx.queue(), n);
 
-    Reductor<cl_float2, SUM> sum(ctx.queue());
-    #define rms(e) (100 * std::sqrt(hsum(sum(pow(e, 2))) / n) / range)
+    Reductor<T, SUM> sum(ctx.queue());
+    #define rms(a,b) (std::sqrt(sum(dot(a - b, a - b))) / std::sqrt(sum(dot(b, b))))
 
     try {
-        FFT<cl_float2>  fft(ctx.queue(), ns);
+        FFT<T2>  fft(ctx.queue(), ns);
         output = fft(input);
 
         bool rc = true;
-        const float rms_fft = rms(output - ref);
+        const T rms_fft = rms(output, ref);
         rc &= rms_fft < 0.1;
 
-        FFT<cl_float2> ifft(ctx.queue(), ns, inverse);
+        FFT<T2> ifft(ctx.queue(), ns, inverse);
         back = ifft(output);
-        const float rms_inv = rms(input - back);
-        rc &= rms_inv < 1e-4;
+        const T rms_inv = rms(back, input);
+        rc &= rms_inv < 1e-3;
 
         std::cout << (rc ? " success." : " failed.") << '\n';
 
-        std::cout << "  fftw-clfft      " << rms_fft << "%" << '\n';
-        std::cout << "  x-ifft(fft(x))  " << rms_inv << "%" << '\n';
-
-        std::cout << fft.plan << '\n';
+        if(!rc) {
+            std::cout << "  fftw-clfft      " << rms_fft << "%" << '\n';
+            std::cout << "  x-ifft(fft(x))  " << rms_inv << "%" << '\n';
+            std::cout << fft.plan << '\n';
+        }
 
         return rc;
 
@@ -94,13 +97,14 @@ double skew_rand(double p) {
 int main() {
     Context ctx(Filter::Env && Filter::Count(1), CL_QUEUE_PROFILING_ENABLE);
     std::cout << ctx << std::endl;
+    srand(23);
 
     bool rc = true;
 
     const size_t max = 1 << 20;
 
     fft::default_planner p;
-    for(size_t i = 0 ; i < 20 ; i++) {
+    for(size_t i = 0 ; i < 100 ; i++) {
         // random number of dimensions, mostly 1.
         size_t dims = 1 + size_t(skew_rand(3) * 5);
         // random size.
@@ -109,7 +113,8 @@ int main() {
         size_t total = 1;
         for(size_t d = 0 ; d < dims ; d++) {
             size_t sz = 1 + size_t(skew_rand(dims == 1 ? 3 : 1) * d_max);
-            sz = p.best_size(sz);
+            if(rand() % 7 != 0)
+                sz = p.best_size(sz);
             n.push_back(sz);
             total *= sz;
         }
