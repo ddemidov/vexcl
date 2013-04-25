c15e9ace17ff626d17f4b79d42d8c613f44ca918
Denis Demidov <ddemidov@ksu.ru>
2013-04-25 22:31:33 +0400

    Revert "A temporary fix for combined expression test in vector_arithmetics"
    This reverts commit 9e243394fbfaf32153033749fccfc7e75484f532.

--- tests/vector_arithmetics.cpp
+++ tests/vector_arithmetics.cpp
@@ -154,10 +154,7 @@ BOOST_AUTO_TEST_CASE(combine_expressions)
 
     vex::vector<int> x(ctx, n);
 
-    double scale = 2 * M_PI;
-    auto   idx   = vex::element_index();
-
-    auto alpha  = scale * idx;
+    auto alpha  = 2 * M_PI * vex::element_index();
     auto sine   = sin(alpha);
     auto cosine = cos(alpha);
 
