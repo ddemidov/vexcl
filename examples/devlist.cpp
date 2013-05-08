24bd9b580a67641098c4c0f94214f54719d9bd4a
Denis Demidov <ddemidov@ksu.ru>
2013-05-08 20:40:59 +0400

    Show device extensions in devlist
    Thanks to @Quanteek for the idea.

--- examples/devlist.cpp
+++ examples/devlist.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <sstream>
+#include <iterator>
 #include <vexcl/devlist.hpp>
 using namespace vex;
 
-
 int main() {
-    std::cout << "OpenCL devices:" << std::endl << std::endl;
+    using namespace std;
+
+    cout << "OpenCL devices:" << endl << endl;
     auto dev = device_list(Filter::All);
     for (auto d = dev.begin(); d != dev.end(); d++) {
-        std::cout << "  " << d->getInfo<CL_DEVICE_NAME>() << std::endl
-                  << "    " << std::left << std::setw(32)
-                  << "CL_PLATFORM_NAME" << " = "
-                  << cl::Platform(d->getInfo<CL_DEVICE_PLATFORM>()).getInfo<CL_PLATFORM_NAME>()
-                  << std::endl;
+        cout << "  " << d->getInfo<CL_DEVICE_NAME>() << endl
+             << "    " << left << setw(32) << "CL_PLATFORM_NAME" << " = "
+             << cl::Platform(d->getInfo<CL_DEVICE_PLATFORM>()).getInfo<CL_PLATFORM_NAME>()
+             << endl;
 
 #define SHOW_DEVPROP(name) \
-        std::cout << "    " << std::left << std::setw(32) << #name << " = " \
-                  << d->getInfo< name >() << std::endl
+        cout << "    " << left << setw(32) << #name << " = " \
+                  << d->getInfo< name >() << endl
 
         SHOW_DEVPROP(CL_DEVICE_VENDOR);
         SHOW_DEVPROP(CL_DEVICE_MAX_COMPUTE_UNITS);
@@ -27,7 +29,20 @@ int main() {
 
 #undef SHOW_DEVPROP
 
-        std::cout << std::endl;
+        cout << "    " << left << setw(32) << "CL_DEVICE_EXTENSIONS" << " = ";
+        {
+            istringstream iss(d->getInfo<CL_DEVICE_EXTENSIONS>());
+            size_t w = 40;
+            for(auto s = istream_iterator<string>(iss); s != istream_iterator<string>(); ++s) {
+                w += s->length() + 1;
+                if (w > 80) {
+                    cout << endl << setw(w = 8) << "";
+                    w += s->length() + 1;
+                }
+                cout << *s << " ";
+            }
+        }
+        cout << endl;
     }
 }
 
