5da1877a4ec18b8d5c9e43f0c08c7444d1b81d7a
Denis Demidov <ddemidov@ksu.ru>
2013-10-08 16:26:39 +0400

    Example of using raw_pointer(vector) together with dereference operator

--- tests/vector_pointer.cpp
+++ tests/vector_pointer.cpp
@@ -3,6 +3,9 @@
 #include <vexcl/vector.hpp>
 #include <vexcl/vector_pointer.hpp>
 #include <vexcl/element_index.hpp>
+#include <vexcl/tagged_terminal.hpp>
+#include <vexcl/temporary.hpp>
+#include <vexcl/constants.hpp>
 #include "context_setup.hpp"
 
 BOOST_AUTO_TEST_CASE(nbody)
@@ -30,6 +33,39 @@ BOOST_AUTO_TEST_CASE(nbody)
             for(size_t i = 0; i < n; ++i)
                 if (i != idx) sum += X[i];
             BOOST_CHECK_CLOSE(v, sum, 1e-8);
+            });
+}
+
+BOOST_AUTO_TEST_CASE(manual_stencil)
+{
+    const size_t n = 1024;
+
+    std::vector<cl::CommandQueue> queue(1, ctx.queue(0));
+
+    std::vector<double> X = random_vector<double>(n);
+
+    vex::vector<double> x(queue, X);
+    vex::vector<double> y(queue, n);
+
+    auto p = vex::tag<1>( vex::raw_pointer(x) );
+    auto N = vex::tag<3>( n );
+
+    VEX_CONSTANT(nil, 0);
+    VEX_CONSTANT(one, 1);
+    VEX_CONSTANT(two, 2);
+
+    auto i   = vex::make_temp<0>( vex::element_index() );
+    auto x_l = vex::make_temp<1>( if_else(i > nil(), *(p + i - one()), *(p + i) ) );
+    auto x_c = vex::make_temp<2>( *(p + i) );
+    auto x_r = vex::make_temp<3>( if_else(i + one() < N, *(p + i + one()), *(p + i) ) );
+
+    y = (two() * x_c - x_l - x_r);
+
+    check_sample(y, [&](size_t idx, double v) {
+            double xc = X[idx];
+            double xl = X[idx > 0 ? idx - 1 : idx];
+            double xr = X[idx + 1 < n ? idx + 1 : idx];
+            BOOST_CHECK_CLOSE(v, 2 * xc - xr - xl, 1e-8);
             });
 }
 
--- vexcl/vector_pointer.hpp
+++ vexcl/vector_pointer.hpp
@@ -1,6 +1,36 @@
 #ifndef VEXCL_VECTOR_POINTER_HPP
 #define VEXCL_VECTOR_POINTER_HPP
 
+/*
+The MIT License
+
+Copyright (c) 2012-2013 Denis Demidov <ddemidov@ksu.ru>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/**
+ * \file   vexcl/vector_pointer.hpp
+ * \author Denis Demidov <ddemidov@ksu.ru>
+ * \brief  Cast vex::vector to a raw pointer.
+ */
+
 #include <vexcl/vector.hpp>
 #include <vexcl/operations.hpp>
 
@@ -8,11 +38,20 @@ namespace vex {
 
 template <typename T>
 struct vector_pointer {
+    typedef T value_type;
+
     const vector<T> &v;
 
     vector_pointer(const vector<T> &v) : v(v) {}
 };
 
+/// Cast vex::vector to a raw pointer.
+/**
+ * Useful when user wants to get a pointer to a vector instead of its current
+ * element inside a vector expression. Could be combined with calls to
+ * address_of/dereference operators or with user-defined functions iterating
+ * through the vector. See examples in tests/vector_pointer.cpp.
+ */
 template <typename T>
 #ifdef DOXYGEN
 vector_pointer<T>
