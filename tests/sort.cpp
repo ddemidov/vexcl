7f8105e5c4aec12ec02df55bd7273d372d1c56a7
Denis Demidov <ddemidov@ksu.ru>
2013-11-26 09:50:32 +0400

    Test sorting of float keys

--- tests/sort.cpp
+++ tests/sort.cpp
@@ -16,32 +16,27 @@ BOOST_AUTO_TEST_CASE(sort_pairs)
     vex::vector<int  > keys(queue, k);
     vex::vector<float> vals(queue, random_vector<float>(n));
 
-    try {
-        vex::sort_by_key(keys, vals);
-        vex::copy(keys, k);
+    vex::sort_by_key(keys, vals);
+    vex::copy(keys, k);
 
-        BOOST_CHECK( std::is_sorted(k.begin(), k.end()) );
+    BOOST_CHECK( std::is_sorted(k.begin(), k.end()) );
 
-        VEX_FUNCTION(even_first, bool(int, int),
-                "char bit1 = 1 & prm1;\n"
-                "char bit2 = 1 & prm2;\n"
-                "if (bit1 == bit2) return prm1 < prm2;\n"
-                "return bit1 < bit2;\n"
-                );
+    VEX_FUNCTION(even_first, bool(int, int),
+            "char bit1 = 1 & prm1;\n"
+            "char bit2 = 1 & prm2;\n"
+            "if (bit1 == bit2) return prm1 < prm2;\n"
+            "return bit1 < bit2;\n"
+            );
 
-        vex::sort_by_key(keys, vals, even_first);
-        vex::copy(keys, k);
+    vex::sort_by_key(keys, vals, even_first);
+    vex::copy(keys, k);
 
-        BOOST_CHECK(std::is_sorted(k.begin(), k.end(), [](int a, int b) {
-                    char bit1 = 1 & a;
-                    char bit2 = 1 & b;
-                    if (bit1 == bit2) return a < b;
-                    return bit1 < bit2;
-                    }));
-    } catch(const vex::error &e) {
-        std::cout << e << std::endl;
-        throw;
-    }
+    BOOST_CHECK(std::is_sorted(k.begin(), k.end(), [](int a, int b) {
+                char bit1 = 1 & a;
+                char bit2 = 1 & b;
+                if (bit1 == bit2) return a < b;
+                return bit1 < bit2;
+                }));
 }
 
 BOOST_AUTO_TEST_CASE(sort_keys)
@@ -50,35 +45,13 @@ BOOST_AUTO_TEST_CASE(sort_keys)
 
     std::vector<vex::backend::command_queue> queue(1, ctx.queue(0));
 
-    std::vector<int  > k = random_vector<int  >(n);
-    vex::vector<int  > keys(queue, k);
+    std::vector<float> k = random_vector<float>(n);
+    vex::vector<float> keys(queue, k);
 
-    try {
-        vex::sort(keys);
-        vex::copy(keys, k);
+    vex::sort(keys);
+    vex::copy(keys, k);
 
-        BOOST_CHECK( std::is_sorted(k.begin(), k.end()) );
-
-        VEX_FUNCTION(even_first, bool(int, int),
-                "char bit1 = 1 & prm1;\n"
-                "char bit2 = 1 & prm2;\n"
-                "if (bit1 == bit2) return prm1 < prm2;\n"
-                "return bit1 < bit2;\n"
-                );
-
-        vex::sort(keys, even_first);
-        vex::copy(keys, k);
-
-        BOOST_CHECK(std::is_sorted(k.begin(), k.end(), [](int a, int b) {
-                    char bit1 = 1 & a;
-                    char bit2 = 1 & b;
-                    if (bit1 == bit2) return a < b;
-                    return bit1 < bit2;
-                    }));
-    } catch(const vex::error &e) {
-        std::cout << e << std::endl;
-        throw;
-    }
+    BOOST_CHECK( std::is_sorted(k.begin(), k.end()) );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
