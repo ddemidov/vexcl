66d6c203d4dabfec09c866efacb3f2ffc325f83b
Denis Demidov <dennis.demidov@gmail.com>
2016-03-21 14:35:50 +0300

    Adding vex::enqueue() -- an assignment expression wrapper
    Allows to explicitly specify the command queues to submit the assignment
    into. By default the queues associated with the vector being assigned
    are used.

--- tests/events.cpp
+++ tests/events.cpp
@@ -3,6 +3,7 @@
 #include <boost/test/unit_test.hpp>
 #include <vexcl/vector.hpp>
 #include <vexcl/reductor.hpp>
+#include <vexcl/enqueue.hpp>
 #include "context_setup.hpp"
 
 BOOST_AUTO_TEST_CASE(compute_overlap)
@@ -25,6 +26,30 @@ BOOST_AUTO_TEST_CASE(compute_overlap)
     vex::backend::enqueue_barrier(q2[0], {vex::backend::enqueue_marker(q1[0])});
 
     y = x;
+
+    BOOST_CHECK_EQUAL(count(y != 2), 0);
+}
+
+BOOST_AUTO_TEST_CASE(assignment_queue)
+{
+    const size_t n = 1024;
+
+    std::vector<vex::command_queue> q1(1, ctx.queue(0));
+    std::vector<vex::command_queue> q2(1, vex::backend::duplicate_queue(ctx.queue(0)));
+
+    vex::vector<int> x(q1, n);
+    vex::vector<int> y(q1, n);
+
+    vex::Reductor<size_t> count(q2);
+
+    x = 1;
+    q1[0].finish();
+
+    x = 2;
+
+    vex::backend::enqueue_barrier(q2[0], {vex::backend::enqueue_marker(q1[0])});
+
+    enqueue(q2, y) = x;
 
     BOOST_CHECK_EQUAL(count(y != 2), 0);
 }
