7e9307956898d0cb74b6f8dc31c357e0cde6336c
Denis Demidov <dennis.demidov@gmail.com>
2016-11-30 10:36:00 +0300

    Implement dummy device/context/command_queue for the JIT backend
    This is enough functionality to compile examples/devlist.cpp

---
+++ vexcl/backend/jit.hpp
@@ -0,0 +1,13 @@
+#ifndef VEXCL_BACKEND_JIT_HPP
+#define VEXCL_BACKEND_JIT_HPP
+
+#ifndef VEXCL_BACKEND_JIT
+#  define VEXCL_BACKEND_JIT
+#endif
+
+#include <vexcl/backend/jit/context.hpp>
+#include <vexcl/backend/jit/filter.hpp>
+#include <vexcl/backend/jit/source.hpp>
+#include <vexcl/backend/jit/kernel.hpp>
+
+#endif
---
+++ vexcl/backend/jit/kernel.hpp
@@ -0,0 +1,15 @@
+#ifndef VEXCL_BACKEND_JIT_KERNEL_HPP
+#define VEXCL_BACKEND_JIT_KERNEL_HPP
+
+namespace vex {
+namespace backend {
+namespace jit {
+
+class kernel {
+};
+
+} // namespace jit
+} // namespace backend
+} // namespace vex
+
+#endif
