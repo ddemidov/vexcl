77a5ee34971345a396fbbe9869decf2190efa3e8
Denis Demidov <dennis.demidov@gmail.com>
2016-12-05 10:28:15 +0300

    Missing headings for the JIT backend files

--- vexcl/backend/jit/source.hpp
+++ vexcl/backend/jit/source.hpp
@@ -1,6 +1,36 @@
 #ifndef VEXCL_BACKEND_JIT_SOURCE_HPP
 #define VEXCL_BACKEND_JIT_SOURCE_HPP
 
+/*
+The MIT License
+
+Copyright (c) 2012-2016 Denis Demidov <dennis.demidov@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/**
+ * \file   vexcl/backend/jit/source.hpp
+ * \author Denis Demidov <dennis.demidov@gmail.com>
+ * \brief  Source code generation for the JIT backend.
+ */
+
 #include <map>
 #include <string>
 #include <iostream>
