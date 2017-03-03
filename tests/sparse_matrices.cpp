b8fe3f52fd70c32c36e19303db168d5bd8c40d00
Denis Demidov <dennis.demidov@gmail.com>
2017-03-03 09:41:36 +0300

    Test matrix-vector product with custom value types

--- tests/sparse_matrices.cpp
+++ tests/sparse_matrices.cpp
@@ -5,6 +5,61 @@
 #include <vexcl/sparse/ell.hpp>
 #include <vexcl/sparse/matrix.hpp>
 #include <vexcl/sparse/distributed.hpp>
+
+typedef std::array<std::array<double, 2>, 2> matrix_value;
+typedef std::array<double, 2> vector_value;
+
+matrix_value mconst(double c) {
+    matrix_value a = {c, c, c, c};
+    return a;
+}
+
+vector_value vconst(double c) {
+    vector_value v = {c, c};
+    return v;
+}
+
+namespace vex {
+
+template <> struct is_cl_native< matrix_value > : std::true_type {};
+template <> struct is_cl_native< vector_value > : std::true_type {};
+
+template <> struct type_name_impl<matrix_value> {
+    static std::string get() { return "double4"; }
+};
+
+template <> struct type_name_impl<vector_value> {
+    static std::string get() { return "double2"; }
+};
+
+namespace sparse {
+
+template <>
+struct rhs_of< matrix_value > {
+    typedef vector_value type;
+};
+
+template <>
+struct spmv_ops_impl<matrix_value, vector_value> {
+    static void decl_accum_var(backend::source_generator &src, const std::string &name)
+    {
+        src.new_line() << type_name<vector_value>() << " " << name << " = {0,0};";
+    }
+
+    static void append_product(backend::source_generator &src,
+            const std::string &sum, const std::string &mat_val, const std::string &vec_val)
+    {
+        src.open("{");
+        src.new_line() << type_name<vector_value>() << " v = " << vec_val << ";";
+        src.new_line() << sum << ".x += " << mat_val << ".x * v.x + " << mat_val << ".y * v.y;";
+        src.new_line() << sum << ".y += " << mat_val << ".z * v.x + " << mat_val << ".w * v.y;";
+        src.close("}");
+    }
+};
+
+} // namespace sparse
+} // namespace vex
+
 #include "context_setup.hpp"
 #include "random_matrix.hpp"
 
@@ -180,5 +235,51 @@ BOOST_AUTO_TEST_CASE(distributed_single)
         BOOST_CHECK_CLOSE(y, sum, 1e-8);
     }
 }
+
+BOOST_AUTO_TEST_CASE(custom_values)
+{
+    const size_t n = 1024;
+    std::vector<vex::command_queue> q(1, ctx.queue(0));
+
+    std::vector<int> ptr, col;
+    std::vector<matrix_value> val;
+
+    ptr.push_back(0);
+    for(size_t i = 0; i < n; ++i) {
+        if (i > 0) {
+            col.push_back(i-1);
+            val.push_back(mconst(-1));
+        }
+        col.push_back(i);
+        val.push_back(mconst(2));
+        if (i + 1 < n) {
+            col.push_back(i+1);
+            val.push_back(mconst(-1));
+        }
+
+        ptr.push_back(col.size());
+    }
+
+    vex::sparse::matrix<matrix_value> A(q, n, n, ptr, col, val);
+
+    std::vector<vector_value> x(n, vconst(1));
+    vex::vector<vector_value> X(q, x);
+    vex::vector<vector_value> Y(q, n);
+
+    Y = A * X;
+
+    for(size_t i = 0; i < n; ++i) {
+        vector_value y = Y[i];
+        vector_value sum = {0,0};
+        for(int j = ptr[i]; j < ptr[i + 1]; j++) {
+            sum[0] += val[j][0][0] * x[col[j]][0] + val[j][0][1] * x[col[j]][1];
+            sum[1] += val[j][1][0] * x[col[j]][0] + val[j][1][1] * x[col[j]][1];
+        }
+
+        BOOST_CHECK_CLOSE(y[0], sum[0], 1e-8);
+        BOOST_CHECK_CLOSE(y[1], sum[1], 1e-8);
+    }
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
