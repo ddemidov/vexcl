46bab619813680f9eb212d408fe831dfef6a513c
Denis Demidov <dennis.demidov@gmail.com>
2017-04-23 21:41:45 +0300

    Fix .travis/install_boost.sh

--- .travis/install_boost.sh
+++ .travis/install_boost.sh
@@ -3,6 +3,7 @@
 export BOOST_ROOT=${HOME}/${BOOST_BASENAME}
 
 if [ ! -e ${BOOST_ROOT}/boost/config.hpp ]; then
+    cd ${HOME}
     wget https://downloads.sourceforge.net/project/boost/boost/1.62.0/${BOOST_BASENAME}.tar.bz2
     rm -rf $BOOST_BASENAME
     tar xf ${BOOST_BASENAME}.tar.bz2
