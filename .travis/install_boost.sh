4ce5bd6d789c510064a06a490bba7c5b5acdffd5
Denis Demidov <dennis.demidov@gmail.com>
2018-03-20 23:29:33 +0300

    Switch to Boost 1.66 on Travis (#236)
    * enable boost compilation on OSX,
    * suppress output from boost compilation,
    * build only what's necessary in boost,
    * remove OSX/compute from allowed failures

--- .travis/install_boost.sh
+++ .travis/install_boost.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 export BOOST_ROOT=${HOME}/${BOOST_BASENAME}
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${BOOST_ROOT}/stage/lib
+fi
 
 if [ ! -e ${BOOST_ROOT}/boost/config.hpp ]; then
     pushd ${HOME}
-    wget https://downloads.sourceforge.net/project/boost/boost/1.62.0/${BOOST_BASENAME}.tar.bz2
+    wget https://downloads.sourceforge.net/project/boost/boost/1.66.0/${BOOST_BASENAME}.tar.bz2
     rm -rf $BOOST_BASENAME
     tar xf ${BOOST_BASENAME}.tar.bz2
-    (cd $BOOST_BASENAME && ./bootstrap.sh && ./b2)
+    (cd ${BOOST_BASENAME} && ./bootstrap.sh --with-libraries=chrono,date_time,filesystem,program_options,system,thread,test && ./b2)
     popd
 fi
 
