cbe82d3e7419428f9b56e31ad82afb894b9678d9
Henry Schreiner <hschrein@cern.ch>
2018-05-17 04:25:34 -0400

    Reordering travis build

--- .travis/install_boost.sh
+++ .travis/install_boost.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 export BOOST_ROOT=${HOME}/${BOOST_BASENAME}
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${BOOST_ROOT}/stage/lib
-fi
 
 if [ ! -e ${BOOST_ROOT}/boost/config.hpp ]; then
     pushd ${HOME}
-    wget https://downloads.sourceforge.net/project/boost/boost/1.66.0/${BOOST_BASENAME}.tar.bz2
+    BOOST_VERSION=$(echo $BOOST_BASENAME | awk -F '_' '{print $2 "." $3 "." $4 }')
+    wget https://dl.bintray.com/boostorg/release/$BOOST_VERSION/source/$BOOST_BASENAME.tar.bz2
     rm -rf $BOOST_BASENAME
     tar xf ${BOOST_BASENAME}.tar.bz2
     (cd ${BOOST_BASENAME} && ./bootstrap.sh --with-libraries=chrono,date_time,filesystem,program_options,system,thread,test && ./b2)
