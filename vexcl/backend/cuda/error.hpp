ddemidov/vexcl

commit 84db53fb10835d6c6ff3870389c63510906013b9

Include error code into error message for unknown errors

vexcl/backend/cuda/error.hpp
@@ -103,7 +103,7 @@ inline std::ostream& operator<<(std::ostream &os, CUresult rc) {
         VEXCL_CUDA_ERR2TXT(CUDA_ERROR_NOT_SUPPORTED);
         VEXCL_CUDA_ERR2TXT(CUDA_ERROR_UNKNOWN);
         default:
-            os << "Unknown error";
+            os << "Unknown error " << rc;
     }
 #undef VEXCL_CUDA_ERR2TXT
     return os << ")";

commit b425063be2666a03ea8d5ce53599ffd73cf66ccf

Fix infinite operator<< loop in cuda/error.hpp
regression from 84db53fb10835d6c6ff3870389c63510906013b9

vexcl/backend/cuda/error.hpp
@@ -103,7 +103,7 @@ inline std::ostream& operator<<(std::ostream &os, CUresult rc) {
         VEXCL_CUDA_ERR2TXT(CUDA_ERROR_NOT_SUPPORTED);
         VEXCL_CUDA_ERR2TXT(CUDA_ERROR_UNKNOWN);
         default:
-            os << "Unknown error " << rc;
+            os << "Unknown error " << static_cast<int>(rc);
     }
 #undef VEXCL_CUDA_ERR2TXT
     return os << ")";